600f4252ef178648c9fce29183d34549fff5f2a8
anton <anton.vasilenko00@gmail.com>
2022-08-13 13:51:41 +0300

    refactor: rename db url env var

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -3,7 +3,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm'
 export default {
   type: process.env.DB_TYPE || 'postgres',
   url:
-    process.env.DB_URL ||
+    process.env.DATABASE_URL ||
     'postgres://postgres:postgres@localhost:5432/finance-management-app-db',
   entities: ['dist/**/*.entity.js'],
   synchronize: true,
